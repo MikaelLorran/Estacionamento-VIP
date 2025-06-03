0a55b07175fdc4a5a9733c0efeb6d41a51817d72
MikaelLorran <kaellorran@gmail.com>
2025-06-03 20:55:30 -0300

    Preparação para deploy

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-	baseURL: "http://localhost/Estacionamento/backend/public/api",
+	baseURL: "http://findspot.free.nf/backend/public/api",
 	withCredentials: true,
 	headers: {
 		"Content-Type": "application/json",
