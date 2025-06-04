7c0a2dca44cdad5fe6a972fd1c7bb698d47198e3
MikaelLorran <kaellorran@gmail.com>
2025-06-03 22:48:52 -0300

    Correções API

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-	baseURL: "https://findspot.free.nf/backend/public/api",
+	baseURL: "https://corsproxy.io/?https://findspot.free.nf/backend/public/api",
 	withCredentials: true,
 	headers: {
 		"Content-Type": "application/json",
